3831c8eb49b300f33cd84aaabf534806cbbf9247
Rano Veder <rano.veder@gmail.com>
2019-02-17 22:24:16 +0100

    Build system created

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Bebop-Data-Logger
+# Bebop Data Logger
+
+This project was created to be able to log stereo vision images from the intel realsense D435 and inertial state data from the Parrot bebop 2 on a single computer.
+
+License
+----
+
+MIT
+
---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <librealsense2/rs.hpp>
+#include <iostream>
+
+void main()
+{
+	rs2::context ctx;
+	auto list = ctx.query_devices(); // Get a snapshot of currently connected devices
+	if (list.size() == 0)
+		throw std::runtime_error("No device detected. Is it plugged in?");
+	
+	rs2::config cfg;
+	cfg.enable_stream(RS2_STREAM_INFRARED, 0);
+	cfg.enable_stream(RS2_STREAM_INFRARED, 1);
+
+	rs2::pipeline pipe;
+	pipe.start();
+
+
+	for (int i = 0; i < 30; i++) pipe.wait_for_frames();;
+	while (true)
+	{
+		auto frame = pipe.wait_for_frames();
+		auto a = frame.get_color_frame().get_bits_per_pixel();
+		//auto d = a.get_profile().stream_type();
+		std::cout << a << std::endl;
+
+	}
+
+}
